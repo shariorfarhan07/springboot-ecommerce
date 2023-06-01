f571301e4ef0f78184c5666ce4cff56af5667d70
shariorfarhan07 <118646614+shariorfarhan111@users.noreply.github.com>
2023-06-01 16:17:26 +0600

    testing cookie

--- src/main/java/com/example/hibarnet_testing/controllers/testController.java
+++ src/main/java/com/example/hibarnet_testing/controllers/testController.java
@@ -1,7 +1,24 @@
 package com.example.hibarnet_testing.controllers;
 
+import aj.org.objectweb.asm.ConstantDynamic;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class testController {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String cookie(HttpServletResponse response){
+        jakarta.servlet.http.Cookie cookie1 =new Cookie("farhan","isThebest");
+        response.addCookie(cookie1);
+
+        return  "test";
+    }
 }
