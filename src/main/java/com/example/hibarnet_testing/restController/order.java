dd323d0038438fa2a1cb7e495f58afddf4ccacf1
shariorfarhan07 <118646614+shariorfarhan111@users.noreply.github.com>
2023-06-17 05:06:52 +0600

    updated all the controller with bodyless method

--- src/main/java/com/example/hibarnet_testing/restController/order.java
+++ src/main/java/com/example/hibarnet_testing/restController/order.java
@@ -1,8 +1,18 @@
 package com.example.hibarnet_testing.restController;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class order {
+    @PostMapping("/order")
+    public String creatOrder(){ return "";}
+    @PutMapping("/order")
+    public String updateOrder(){ return "";}
+
+    @PutMapping("/order/{id}")
+    public String stateChangeOrder(){ return "";}
+
 
 }
